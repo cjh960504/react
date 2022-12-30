c5e938fbbc94ebdf57271c894e68bd348fca8537
hyukhub <57378009+cjh960504@users.noreply.github.com>
2022-12-30 18:29:43 +0900

    5.props를 통해 컴포넌트에게 값 전달하기 end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
+import Hello from './Hello';
+import Wrapper from './Wrapper';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  /*Javascript 영역*/
+  const name = "준혁";
+  const style = {
+    backgroundColor : 'black',
+    color : 'aqua',
+    fontSize : 24,
+    padding : '1rem'
+  };
+
+
+  return ( /*
+            JSX 영역
+           */
+      <Wrapper>
+        {/*주석*/}
+        /*보여유*/
+        <Hello 
+          name={name} 
+          color="red"
+          //태그 내부에는 한줄주석가능
+        />
+        <Hello 
+          color="red"
+          //태그 내부에는 한줄주석가능
+        />
+        <div style={style}>{name}</div>
+        <div className="gray-box"></div>
+      </Wrapper>
   );
 }
 
